e5bf8a7706c18f6b211080dd46e9f18988b36032
Donna <donnatopia@gmail.com>
2022-11-30 10:42:47 -0800

    passed sum() tests

--- src/recursion.js
+++ src/recursion.js
@@ -21,6 +21,15 @@ var factorial = function(n) {
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+  var total = 0;
+
+  if (array.length > 1) {
+    total += array[0] + sum(array.slice(1))
+  } else if (array.length === 1){
+    total += array[0];
+  }
+
+  return total;
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
