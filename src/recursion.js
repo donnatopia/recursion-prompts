73c79d16ffcaf8280d4f537ce16208bf9d0a5d6b
Donna <donnatopia@gmail.com>
2022-11-30 11:07:01 -0800

    passed arraySum() tests

--- src/recursion.js
+++ src/recursion.js
@@ -35,6 +35,17 @@ var sum = function(array) {
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+  var total = 0;
+
+  if (Array.isArray(array)) {
+    for (var i = 0; i < array.length; i++) {
+      total += arraySum(array[i]);
+    }
+  } else {
+    total += array;
+  }
+
+  return total;
 };
 
 // 4. Check if a number is even.
