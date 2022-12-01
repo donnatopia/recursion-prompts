829d37b88a2ea0b72704c194b4ff6989dfaeff4c
Donna <donnatopia@gmail.com>
2022-11-30 18:08:54 -0800

    passed palindrome() tests

--- src/recursion.js
+++ src/recursion.js
@@ -137,6 +137,23 @@ var reverse = function(string) {
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+  if (string.length <= 1) {
+    return true;
+  } else {
+    string = string.toLowerCase()
+                 .match(/[a-z]/g)
+                 .join('');
+
+    var first = string[0];
+    var last = string[string.length - 1];
+
+    if (first === last) {
+      var newString = string.substring(1, string.length - 1);
+      return palindrome(newString);
+    } else {
+      return false;
+    }
+  }
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
