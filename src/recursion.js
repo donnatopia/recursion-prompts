b81ff892bf5ff8293438d4896765a5ac8c8b822f
Donna <donnatopia@gmail.com>
2022-12-02 12:26:30 -0800

    passed gcd() tests

--- src/recursion.js
+++ src/recursion.js
@@ -242,6 +242,16 @@ var divide = function(x, y) {
 // http://www.cse.wustl.edu/~kjg/cse131/Notes/Recursion/recursion.html
 // https://www.khanacademy.org/computing/computer-science/cryptography/modarithmetic/a/the-euclidean-algorithm
 var gcd = function(x, y) {
+  if (x === 0) {
+    return y;
+  } else if (y === 0) {
+    return x;
+  } else if (x < 0 || y < 0) {
+    return null;
+  }
+
+  var remainder = x % y;
+  return gcd(y, remainder);
 };
 
 // 15. Write a function that compares each character of two strings and returns true if
